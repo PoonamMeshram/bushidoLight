8260b49d65bc4e49f71ecd0c651c1d7ef05bbedb
Poonam <poonam.meshram@geeconsystems.com>
2024-01-18 20:50:21 +0530

    push commit

--- src/test/java/com/bushidolight/testCases/BaseClass.java
+++ src/test/java/com/bushidolight/testCases/BaseClass.java
@@ -3,20 +3,14 @@ package com.bushidolight.testCases;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.devtools.idealized.Javascript;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
-import org.testng.annotations.Test;
-
 import org.testng.annotations.Parameters;
 
 import bushido.config.PropertiesFile;
